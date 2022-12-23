6cfac8a04ebf1e3d928b2d8fd71fc7d434f0b36f
Lovely Jane Garlan <90582377+Garlan18@users.noreply.github.com>
2022-12-23 20:42:28 +0800

    modified: index.js

--- Chat App - Garlan/src/index.js
+++ Chat App - Garlan/src/index.js
@@ -7,3 +7,8 @@ const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
+
+app.listen(port, () => {
+    console.log ('Server is up on port $(port)!')
+
+})
