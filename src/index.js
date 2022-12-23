d129838e3cda61e52a4df13115a548de0635d382
Lovely Jane Garlan <90582377+Garlan18@users.noreply.github.com>
2022-12-23 21:53:27 +0800

    modified

--- src/index.js
+++ src/index.js
@@ -16,12 +16,17 @@ io.on('connection', (socket) => {
     console.log('New Websocket connection')
 
     socket.emit('message', 'Welcome')
+    socket.broadcast.emit('message', 'A new user joined!')
 
     socket.on('sendMessage', (message) => {
         io.emit('message', message)
 
     })
 
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left')
+
+    })
 })
 
 server.listen(port, () => {
